b747618daeff2ff9f33e5ed7c7ce103a01a7e100
Greg Houston <ghouston@yahoo.com>
2015-07-05 13:55:30 -0500

    updated WaterDrop prefab size. added Greg scene (copied from Scott). added RainMaker.cs (similar to Cloud.cs) which uses a rainSource (game object with box collider 2d) to determine where to start the rain falling, the rain will fall randomly from along the x width of the collider.  RainMaker.cs also adds an initial velocity so that raindrops fall with a better initial velocity.

---
+++ cloud/Assets/Scripts/RainMaker.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainMaker : MonoBehaviour
+{
+
+	public GameObject rainSource;
+	public GameObject rainDrop;
+	public float rainRate = 0.1f;
+	public float initialVelocity = 0.0f;
+
+	private float rainSourceWidth;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rainSourceWidth = rainSource.GetComponent<BoxCollider2D> ().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Jump") || Input.GetKeyDown ("space")) {
+			InvokeRepeating ("MakeRain", 0.0f, rainRate);
+		}
+		if (Input.GetButtonUp ("Jump")) {
+			CancelInvoke ("MakeRain");
+		}
+	}
+	
+	void MakeRain ()
+	{
+		Vector3 spawnPosition = new Vector3 (
+				rainSource.transform.position.x + Random.Range (-rainSourceWidth, rainSourceWidth),
+				rainSource.transform.position.y,
+				0.0f
+		);
+		GameObject clone = (GameObject)Instantiate (rainDrop, spawnPosition, Quaternion.identity);
+		Rigidbody2D rb2d = clone.GetComponent<Rigidbody2D> ();
+		rb2d.AddForce (new Vector2 (0.0f, -initialVelocity));
+	}
+}
